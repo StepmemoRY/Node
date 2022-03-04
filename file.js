d033cb6d193f292043dc2d2ef7b57a40a54b9eff
StepmemoRY <mehm12332@gmail.com>
2022-03-04 16:24:48 +0300

    node 2

--- file.js
+++ file.js
@@ -1,6 +1,8 @@
 
 const fs = require("fs")
+const { resolve } = require("path")
 const path = require("path")
+
 
 //fs.mkdirSync(path.resolve(__dirname ,"dir")) 
 
@@ -66,24 +68,65 @@ fs.writeFile(path.resolve(__dirname,"dir.txt"), "0",(err) =>{
 
 //Promise
 
+
 const writeFileAsync = async (path,data) => {
-    return new Promise((resolve ,reject) => fs.writeFile(path,data),(err) =>{
+    return new Promise((resolve ,reject) => fs.writeFile(path,data,(err) =>{
         if(err){
             return reject(err.message)
         }
         resolve()
-    })
+    }))
 }
 const appendFileAsync = async (path,data) => {
-    return new Promise((resolve ,reject) => fs.writeFile(path,data),(err) =>{
+    return new Promise((resolve ,reject) => fs.appendFile(path,data,(err) =>{
         if(err){
             return reject(err.message)
         }
         resolve()
-    })
+    }))
 }
-writeFileAsync(path.resolve(__dirname,"dir.txt"), 'data')
-    .then(() => appendFileAsync(path.resolve(_dirname,"dir.txt"),"merhaba"))
-    .catch(err => console.log(err))
 
+writeFileAsync(path.resolve(__dirname,"dir.txt"),"merhaba")
+  .then(() => appendFileAsync(path.resolve(__dirname,"dir.txt"),"asdad"))
+  .catch(err => console.log(err))
+
+
+/*
+---------------------------------------------------------------------------------------------------
+const writeFileAsync = async (path,data) => {
+    return new Promise((resolve ,reject) => fs.writeFile(path,data,(err) =>{
+        if(err){
+            return reject(err.message)
+        }
+        resolve()
+    }))
+} 
+
+const text = 'BIL 4 2 5'
+
+const readFileAsync = async (path) => {
+    return new Promise((resolve,reject) => fs.readFile(path, {encoding:'utf-8' }, (err,data) => {
+        if(err){
+            return reject(err.message)
+        }
+        resolve(data)
+    }))
+}
+writeFileAsync(path.resolve(__dirname,"text.txt"),text)
+.then(() => readFileAsync(path.resolve(__dirname,"text.txt")))
+.then(data => data.split(' ').length)
+.then(count => writeFileAsync(path.resolve(__dirname,"count.txt"),`file count ${count}`))
+----------------------------------------------------------------------------------------------------
+*/
+/*
+const fsPromise = require('fs').promises
+
+const fileRead = async () => {
+    try{
+        await fsPromise.writeFile(path.resolve(__dirname, "text.txt"))
+    } catch(error){
+
+    }  
+}
+*/
 
